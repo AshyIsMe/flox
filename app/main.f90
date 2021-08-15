5104a554d368535ddee9eae8174bfd101b2b93c9
Aaron Ash <aaron.ash@gmail.com>
2021-08-15 21:46:40 +1000

    feat: cli args

--- app/main.f90
+++ app/main.f90
@@ -1,10 +1,24 @@
 program main
-  ! use flox, only: runprompt,line_len
   use flox, only: runprompt
+  use M_CLI,  only : commandline, check_commandline
   implicit none
 
-  ! character(len=line_len) :: line
-  ! character(512) :: line
-  call runprompt()
+  character(len=:),allocatable :: readme ! stores updated namelist
+  character(len=256)           :: message
+  integer                      :: ios
+  character(len=256)           :: file
+
+  namelist /args/ file
+     readme=commandline('--file "filename"')
+     read(readme,nml=args,iostat=ios,iomsg=message)
+     call check_commandline(ios,message)
+    !  write(*,*) file
+
+  if (file /= "filename") then
+     write(*,*) 'Running file: ',file
+    !  call runfile(file)
+  else
+    call runprompt()
+  end if
 
 end program main
