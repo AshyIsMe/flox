5e1fb0aac8b9ccce57ce4d97ca0737c0361eb2b2
Aaron Ash <aaron.ash@gmail.com>
2021-09-10 23:10:13 +1000

    add operators

--- src/mod_scanner.f90
+++ src/mod_scanner.f90
@@ -20,6 +20,7 @@ module mod_scanner
     procedure, pass(self) :: isAtEnd
     procedure, pass(self) :: advanceChar
     procedure, pass(self) :: addToken
+    procedure, pass(self) :: match
   end type Scanner
 
 
@@ -46,10 +47,11 @@ contains
   subroutine scanToken(self)
     class(Scanner), intent(inout) :: self
     character :: c
+    logical :: bmatch = .false.
 
     call self%advanceChar(c)
 
-    print *, c
+    !print *, c
     select case (c)
     case ('(')
       call self%addToken(TT_LEFT_PAREN)
@@ -71,6 +73,40 @@ contains
       call self%addToken(TT_SEMICOLON)
     case ('*')
       call self%addToken(TT_STAR)
+
+    case ('!')
+      call self%match('=', bmatch)
+      if (bmatch) then
+        call self%addToken(TT_BANG_EQUAL)
+      else
+        call self%addToken(TT_BANG)
+      end if
+
+    case ('=')
+      call self%match('=', bmatch)
+      if (bmatch) then
+        call self%addToken(TT_EQUAL_EQUAL)
+      else
+        call self%addToken(TT_EQUAL)
+      end if
+
+    case ('<')
+      call self%match('=', bmatch)
+      if (bmatch) then
+        call self%addToken(TT_LESS_EQUAL)
+      else
+        call self%addToken(TT_LESS)
+      end if
+
+    case ('>')
+      call self%match('=', bmatch)
+      if (bmatch) then
+        call self%addToken(TT_GREATER_EQUAL)
+      else
+        call self%addToken(TT_GREATER)
+      end if
+
+
     case default
       call error(self%line, 'Unexpected character.')
     end select
@@ -122,6 +158,23 @@ contains
     end if
 
   end subroutine addToken
+
+  subroutine match(self, c, bmatch)
+    class(Scanner), intent(inout) :: self
+    character, intent(in) :: c
+    logical, intent(out) :: bmatch
+    bmatch = .false.
+
+    if (self%isAtEnd()) then
+      return
+    end if
+    if (c /= self%source(self%current:self%current)) then
+      return
+    end if
+
+    self%current = self%current + 1
+    bmatch = .true.
+  end subroutine match
 
   subroutine error (line, message)
     !According to the book this is in the Lox class instead of Scanner
