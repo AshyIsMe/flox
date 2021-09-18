1c3b6394960fa58b20e33ed7ef8a18bf8a1c4939
Aaron Ash <aaron.ash@gmail.com>
2021-09-18 17:53:39 +1000

    fix: write_token

--- src/mod_token.f90
+++ src/mod_token.f90
@@ -1,11 +1,12 @@
 module mod_token
   use mod_tokentype
+  use M_strings, only: crop
 
   implicit none
 
   type :: Token
     integer(kind(TokenType)) :: token
-    character(32) :: lexeme
+    character(32) :: lexeme ! TODO: use iso_varying_string
     !character(len=:),allocatable :: lexeme
 
     ! Object :: literal ! Can we do something similar to this in Fortran?
@@ -15,20 +16,29 @@ module mod_token
 
   contains
     procedure write_token
-    generic :: write(unformatted) => write_token
+    !generic :: write(unformatted) => write_token
+    generic :: write(formatted) => write_token
   end type Token
 
 contains
 
-  subroutine write_token(dtv, unit, iostat, iomsg)
+  subroutine write_token(dtv, unit, iotype, v_list, iostat, iomsg)
     class(token), intent(in) :: dtv
     integer, intent(in) :: unit
+    character(*), intent(in) :: iotype
+    integer, intent(in) :: v_list(:)
     integer, intent(out) :: iostat
     character(*), intent(inout) :: iomsg
 
     !write(unit, iostat=iostat, iomsg=iomsg) dtv%token
-    ! AA TODO: This doesn't currently seem to work when we print the scanner%tokens array
-    write(unit, iostat=iostat, iomsg=iomsg, fmt=*) 'Token[' // tt_tostring(dtv%token) // ']'
+    !write(unit, iostat=iostat, iomsg=iomsg, fmt='(a)') 'Token[' // tt_tostring(dtv%token) // ']'
+
+    write(unit, iostat=iostat, iomsg=iomsg, fmt='(a)', advance="no") 'Token[' // tt_tostring(dtv%token) // ','
+    !write(unit, iostat=iostat, iomsg=iomsg, fmt='(a)', advance="no") '"' // dtv%lexeme // '",'
+    write(unit, iostat=iostat, iomsg=iomsg, fmt='(a)', advance="no") crop(dtv%lexeme) // ','
+    write(unit, iostat=iostat, iomsg=iomsg, fmt='(a)', advance="no") 'line '
+    write(unit, iostat=iostat, iomsg=iomsg, fmt='(i4)', advance="no") dtv%line
+    write(unit, iostat=iostat, iomsg=iomsg, fmt='(a)') ']'
   end subroutine write_token
 
 end module mod_token
